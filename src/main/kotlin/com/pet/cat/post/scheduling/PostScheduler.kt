4372e48c5fc8a1b66f61f79abdbeae9ca5892556
Jung jinseong <wjdwl545@naver.com>
2025-09-02 21:54:47 +0900

    fix schedule

--- src/main/kotlin/com/pet/cat/post/scheduling/PostScheduler.kt
+++ src/main/kotlin/com/pet/cat/post/scheduling/PostScheduler.kt
@@ -1,15 +1,23 @@
 package com.pet.cat.post.scheduling
 
 import com.pet.cat.post.repository.PostRepository
+import lombok.extern.java.Log
+import lombok.extern.slf4j.Slf4j
+import org.slf4j.LoggerFactory
 import org.springframework.scheduling.annotation.Scheduled
+import org.springframework.stereotype.Component
 import org.springframework.transaction.annotation.Transactional
 
+@Component
 class PostScheduler(
     private val postRepository: PostRepository,
     ) {
-    @Scheduled(cron = "0 0 0 * * *", zone = "Asia/Seoul")
+    private val log = LoggerFactory.getLogger(PostScheduler::class.java)
+
+    @Scheduled(cron = "0 56 21 * * *", zone = "Asia/Seoul")
     @Transactional
     fun recompute() {
+        log.info("[PostScheduler]-recompute start")
         val updatedRows = postRepository.recomputePopularityScore(
             wLike = 1.0,
             wView = 0.3,
