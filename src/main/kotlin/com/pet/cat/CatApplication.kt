59c82598851dbcdb49ade526566c4df1fb6d235d
Jung jinseong <wjdwl545@naver.com>
2025-10-24 00:58:59 +0900

    타임존 설정

--- src/main/kotlin/com/pet/cat/CatApplication.kt
+++ src/main/kotlin/com/pet/cat/CatApplication.kt
@@ -3,11 +3,13 @@ package com.pet.cat
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.scheduling.annotation.EnableScheduling
+import java.util.TimeZone
 
 @SpringBootApplication
 @EnableScheduling
 class CatApplication
 
 fun main(args: Array<String>) {
+	TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
 	runApplication<CatApplication>(*args)
 }
